176ece62199d8542a9629f1963c0cac97f6107de
satzcalvin <sathy2812@gmail.com>
2020-12-17 22:43:03 +0530

    fifth commit

--- src/main/java/com/example/springcicdintegrate/SpringCicdIntegrateApplication.java
+++ src/main/java/com/example/springcicdintegrate/SpringCicdIntegrateApplication.java
@@ -1,4 +1,4 @@
-package com.example.springcicdIntegrate;
+package com.example.springcicdintegrate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
