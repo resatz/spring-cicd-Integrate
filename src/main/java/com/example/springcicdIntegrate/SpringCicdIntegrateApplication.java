2ec9ac770c028545fd5929206a914e7e820c8dd3
satzcalvin <sathy2812@gmail.com>
2020-12-17 20:46:21 +0530

    first commit

---
+++ src/main/java/com/example/springcicdIntegrate/SpringCicdIntegrateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcicdIntegrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdIntegrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCicdIntegrateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcicdIntegrate/SpringCicdIntegrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcicdIntegrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCicdIntegrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
