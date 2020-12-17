9d0fcedf428923e38bc8c4634ae4e1ba057b191a
satzcalvin <sathy2812@gmail.com>
2020-12-17 21:56:41 +0530

    second commit

--- src/main/java/com/example/springcicdIntegrate/SpringCicdIntegrateApplication.java
+++ src/main/java/com/example/springcicdIntegrate/SpringCicdIntegrateApplication.java
@@ -2,10 +2,17 @@ package com.example.springcicdIntegrate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringCicdIntegrateApplication {
 
+	@GetMapping("/app")
+	public String message() {
+		return "hello";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCicdIntegrateApplication.class, args);
 	}
